4b5e7bf6ba70d8a7bddf3b27f76794a3e1b03d7a
Hasan Tanrıverdi <hthasantanriverdi@gmail.com>
2025-05-07 19:56:01 +0300

    Initial commit

---
+++ muzikdukkanıyonetimi/veri.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public static class veri
+{
+    public static List<string> urunListesi = new List<string>()
+    {
+        "Ürün Adı: Gitar - Ürün Türü: Telli - Ürün Sayısı : 9 - Ürün Fiyatı : 6800TL",
+        "Ürün Adı: Keman - Ürün Türü: Telli - Ürün Sayısı : 5 - Ürün Fiyatı : 4200TL",
+        "Ürün Adı: Bateri - Ürün Türü: Vurmalı - Ürün Sayısı : 3 - Ürün Fiyatı : 10500TL",
+        "Ürün Adı: Flüt - Ürün Türü: Nefesli - Ürün Sayısı : 12 - Ürün Fiyatı : 850TL",
+        "Ürün Adı: Saksafon - Ürün Türü: Nefesli - Ürün Sayısı : 2 - Ürün Fiyatı : 9500TL",
+        "Ürün Adı: Davul - Ürün Türü: Vurmalı - Ürün Sayısı : 4 - Ürün Fiyatı : 7500TL",
+        "Ürün Adı: Elektro Gitar - Ürün Türü: Telli - Ürün Sayısı : 6 - Ürün Fiyatı : 8800TL",
+        "Ürün Adı: Klasik Gitar - Ürün Türü: Telli - Ürün Sayısı : 10 - Ürün Fiyatı : 3100TL",
+        "Ürün Adı: Piyano - Ürün Türü: Tuşlu - Ürün Sayısı : 1 - Ürün Fiyatı : 21500TL",
+        "Ürün Adı: Org - Ürün Türü: Tuşlu - Ürün Sayısı : 7 - Ürün Fiyatı : 5600TL",
+        "Ürün Adı: Ud - Ürün Türü: Telli - Ürün Sayısı : 3 - Ürün Fiyatı : 4400TL",
+        "Ürün Adı: Ney - Ürün Türü: Nefesli - Ürün Sayısı : 8 - Ürün Fiyatı : 1200TL",
+        "Ürün Adı: Cajon - Ürün Türü: Vurmalı - Ürün Sayısı : 5 - Ürün Fiyatı : 3100TL",
+        "Ürün Adı: Mandolin - Ürün Türü: Telli - Ürün Sayısı : 2 - Ürün Fiyatı : 3600TL",
+        "Ürün Adı: Klarnet - Ürün Türü: Nefesli - Ürün Sayısı : 6 - Ürün Fiyatı : 5800TL",
+        "Ürün Adı: Akordeon - Ürün Türü: Tuşlu - Ürün Sayısı : 1 - Ürün Fiyatı : 9900TL",
+        "Ürün Adı: Tambur - Ürün Türü: Telli - Ürün Sayısı : 4 - Ürün Fiyatı : 2750TL"
+    };
+}
